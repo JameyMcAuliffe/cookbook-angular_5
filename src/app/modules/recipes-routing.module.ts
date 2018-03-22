82d6c57bc71ba8dad4b9517e4b49087834fc48ce
Jamey McAuliffe <jpmcauliffe15@hotmail.com>
2018-03-22 16:02:40 -0500

    moved modules into folder, created home component, used lazy loading for recipes

--- src/app/modules/recipes-routing.module.ts
+++ src/app/modules/recipes-routing.module.ts
@@ -1,19 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
-import { RecipesComponent } from './recipes/recipes.component';
-import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
-import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
-import { AuthGuard } from './auth/auth-guard.service';
+import { RecipeDetailComponent } from '../recipe-detail/recipe-detail.component';
+import { RecipesComponent } from '../recipes/recipes.component';
+import { RecipeStartComponent } from '../recipes/recipe-start/recipe-start.component';
+import { RecipeEditComponent } from '../recipes/recipe-edit/recipe-edit.component';
+import { AuthGuard } from '../auth/auth-guard.service';
 
 const recipesRoutes: Routes = [
-		{path: 'recipes', component: RecipesComponent, children: [
+	{path: '', component: RecipesComponent, children: [
 		{path: '', component: RecipeStartComponent},
 		{path: 'new', component: RecipeEditComponent, canActivate: [AuthGuard]},
 		{path: ':id', component: RecipeDetailComponent},
 		{path: ':id/edit', component: RecipeEditComponent, canActivate: [AuthGuard]}
-	]},
+	]}
 ]
 
 @NgModule({
--- src/app/modules/recipes.module.ts
+++ src/app/modules/recipes.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
-import { RecipesComponent } from './recipes/recipes.component';
-import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
-import { RecipeListComponent } from './recipe-list/recipe-list.component';
-import { RecipeItemComponent } from './recipe-item/recipe-item.component';
-import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
-import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
+import { RecipesComponent } from '../recipes/recipes.component';
+import { RecipeStartComponent } from '../recipes/recipe-start/recipe-start.component';
+import { RecipeListComponent } from '../recipe-list/recipe-list.component';
+import { RecipeItemComponent } from '../recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from '../recipe-detail/recipe-detail.component';
+import { RecipeEditComponent } from '../recipes/recipe-edit/recipe-edit.component';
 import { RecipesRoutingModule } from './recipes-routing.module';
 import { SharedModule } from './shared.module';
 
